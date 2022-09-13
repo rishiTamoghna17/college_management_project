7e9ff7f2b54011e96a6ba85a25076db8ea29f6cc
rishiTamoghna17 <tamoghna171099@gmail.com>
2022-09-13 16:59:06 +0530

    updated by Tamoghna

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("", {
+mongoose.connect("mongodb+srv://surajkumar96:6i0d4EhtRtZ5xCEQ@cluster0.mqcx8wl.mongodb.net/group_38", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
