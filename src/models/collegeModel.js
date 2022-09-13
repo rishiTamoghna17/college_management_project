e4d7da96f313e6b062bbb8d5bacd061dae745412
komal <ranikomal596@gmail.com>
2022-09-13 17:29:34 +0530

    schema done

---
+++ src/models/collegeModel.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose')
+const collegeSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+         trim:true
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim:true
+
+    },
+    logoLink:{
+        type:String,
+        required:true
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    }},
+    {timestamps: true
+})
+module.exports=mongoose.model('collegeModel',collegeSchema)
---
+++ src/models/internModel.js
@@ -0,0 +1,32 @@
+const mongoose=require('mongoose')
+const collegeModel = require('./collegeModel')
+const ObjectId=mongoose.Schema.Types.ObjectId
+const internModelSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    mobile:{
+        type:Number,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    collegeId:{
+        type:ObjectId,
+        ref:'collegeModel'
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    }},
+    {timestamps: true
+})
+module.exports=mongoose.model('internModel',internModelSchema)
