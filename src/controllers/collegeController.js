a6f5411d8e0023bdf47a0a919277278032e3d593
navdeep <pandatnavvu@gmail.com>
2022-09-14 13:33:04 +0530

    project2

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,26 +1,68 @@
-const { accessSync } = require("fs")
+
 const collegeModel= require("../models/collegeModel")
 const internModel= require("../models/internModel")
-const isValid= function(value){
-    if(typeof value=="undefined" || value==null) return false
-    if(typeof value=="String" && value.trim().length==0)return false
-    return true;
-}
+
 const createCollege = async function (req, res) { 
-    try{
-        let data= req.body;
-        
-    // if (Object.keys(data).length === 0) {
-    //     return res.status(400).send({ status: false, message: "Please give some data" });
-    //   }
-    //     if(!isValid(data.name)){
-    //     return res.status(400).send({status:false,messege:"invalid name"})
-    //     }
-        let savedata = await collegeModel.create(data)
-        res.status(201).send({status:true,data:savedata})
-    } 
-    catch(err){
-        res.status(500).send({msg:err.messege})
-    }
-}
+    
+        try {
+            const data = req.body; 
+
+            // destructure college data
+            let { name, fullName, logoLink, isDeleted, ...rest } = data
+            console.log(data)
+            if (!Object.keys(data).length) return res.status(400).send({ status: false, message: "pls enter the data in body" })
+
+            //do not accept undefiend attributes
+            if (Object.keys(rest).length > 0)
+                  return res.status(400).send({ status: false, message: "pls enter only name, fullname,logoLink" });
+
+            //validate the college name
+            if (!name) return res.status(400).send({ status: false, message: "Name is Missing" })
+             //Check if college full name present or not?
+             if (!fullName) return res.status(400).send({ status: false, message: "Fullname is Missing" })
+
+            //Check if Name Is Vilid or Not?
+            var regEx = /[a-z]+/;
+            if (!regEx.test(name)) {
+                  return res.status(400).send({ status: false, message: "Name is invalid" });
+            }
+
+           
+
+            //Check if fullName Is Vilid or Not?
+            var regEex = /[a-z]+/;
+            if (!regEex.test(fullName)) {
+                  return res.status(400).send({ status: false, message: "fullName is invalid" });
+            }
+
+            //Check if college logoLink present or not?
+            if (!logoLink) return res.status(400).send({ status: false, message: "LogoLink is Missing" })
+
+            // Check the LogoLink Is Valid or Not?
+            if (!(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%\+.~#?&//=]*)/.test(data.logoLink))) {
+                  return res.status(400).send({ status: false, msg: 'Not a valid logoLink' })
+            }
+
+            //check if isDeleted is TRUE/FALSE ?
+            if (isDeleted && (isDeleted === "" || (!(typeof isDeleted == "boolean")))) {
+                  return res.status(400).send({ status: false, message: "isDeleted Must be TRUE OR FALSE" });
+            }
+            if (isDeleted)
+            return res.status(400).send({ status: false, message: "you can not set isdeleted True" });
+
+            //if college name is already present In DB or Not!
+            const findName = await collegeModel.findOne({ name: name })
+            if (findName) return res.status(400).send({ status: false, message: "Name Is Already Present In DB" });
+
+            //create Data In DB
+            let collegeData = await collegeModel.create(data)
+            return res.status(201).send({ status: true, data: collegeData });
+      }
+       catch (err) {
+             return res.status(500).send({ status: false, message: "Error", error: err.message })
+       }
+   
+
+    
+ }
 module.exports.createCollege=createCollege
